c25fd630aa9738d2dbb038208df4562c86879737
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-09-08 13:30:22 -0500

    Update app.js
    video 010

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ console.log('hasta aqui todo bien');
 console.log('true');
 console.log('ID:' + 20);*/
 
-var nombre="Juan"
+//variables en var puden ser declaradas varias veces la misma variable sin problema
+/*var nombre="Juan"
 nombre="JHON"
 var carrito="libro",
  carrito1="libro2";
@@ -18,4 +19,16 @@ console.log(nombre, carrito, carrito1);
 var primerNombre="Juan";
 var primer_nombre="Juan";
 var primernombre="Juan";
-var totalCarrito;
+var totalCarrito;*/
+
+//variables en let diferencia con var es que con let no puedes declarar la misma variable 2 o mas veces, puedes
+//cambiar el valor de la variable pero no volver a declararla
+/*let nombre="Juan",
+producto="Libro";
+
+console.log(nombre);*/
+
+//variables con const la diferencia es que se debe de asignar un valor a la variable a la hora de declarar
+const producto="Libro";
+
+console.log(producto);  
