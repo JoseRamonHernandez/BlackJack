657f29e3b25826c7e8850a08ed249663043d6cee
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-09-08 13:22:40 -0500

    Update app.js
    video 009

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 /*let nombre= prompt('¿Cual es tu nombre?');
 let edad= prompt('¿Cual es tu edad?');
 
-document.getElementById('app').innerHTML= `Bienvenido ${nombre} de ${edad} años`;*/
+document.getElementById('app').innerHTML= `Bienvenido ${nombre} de ${edad} años`;
 
 console.log('2+2');
 console.log('hasta aqui todo bien');
 console.log('true');
-console.log('ID:' + 20);
+console.log('ID:' + 20);*/
+
+var nombre="Juan"
+nombre="JHON"
+var carrito="libro",
+ carrito1="libro2";
+
+console.log(nombre, carrito, carrito1);
+
+var primerNombre="Juan";
+var primer_nombre="Juan";
+var primernombre="Juan";
+var totalCarrito;
