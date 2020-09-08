84e351d8f0b4b8533361be9eafb79c7d8a01ea49
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-09-08 13:03:51 -0500

    Update app.js
    video 007

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let nombre= prompt('¿Cual es tu nombre?');
+/*let nombre= prompt('¿Cual es tu nombre?');
 let edad= prompt('¿Cual es tu edad?');
 
-document.getElementById('app').innerHTML= `Bienvenido ${nombre} de ${edad} años`;
+document.getElementById('app').innerHTML= `Bienvenido ${nombre} de ${edad} años`;*/
+
+console.log('2+2');
+console.log('hasta aqui todo bien');
+console.log('true');
+console.log('ID:' + 20);
